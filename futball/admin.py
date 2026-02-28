99ca3ffe70f1be4b07a05e2d799448c28dcf62f2
dani <plottwist0101@gmail.com>
2026-02-28 12:17:28 +0700

    update dashboard dan team logo

--- futball/admin.py
+++ futball/admin.py
@@ -4,6 +4,7 @@ from futball.models.season import Season
 from futball.models.match import Match, MatchTeamStats
 from futball.models.team import Team
 from futball.models.shots import Shot
+from futball.models.player import Player, PlayerMatch
 from futball.models.news import News, NewsContentImage
 
 admin.site.register(Competition)
@@ -12,5 +13,7 @@ admin.site.register(Team)
 admin.site.register(Match)
 admin.site.register(MatchTeamStats)
 admin.site.register(Shot)
+admin.site.register(Player)
+admin.site.register(PlayerMatch)
 admin.site.register(News)
 admin.site.register(NewsContentImage)
